e08217c0a52c3d52052f0915e1882e453f8d7fee
Ashutosh Tripathi <tripathiashutosh665@gmail.com>
2024-10-14 23:55:54 +0530

    edited

--- src/_array_/ReturnEven.java
+++ src/_array_/ReturnEven.java
@@ -2,18 +2,24 @@ package _array_;
 
 public class ReturnEven {
 	public static void main(String[] args) {
-		int[] arr= {12,13,14,15,16};
+		int[] arr= {12,10,14,16,16};
 		boolean result=evenReturn(arr);
+		if(result) {
+			System.out.println("It is even");
+		}
+		else {
+			System.out.println("It is not even");
+		}
 		
 	}
 	public static boolean evenReturn(int[] arr) {
 		
 		for(int p:arr) {
-			if(p%2==0) {
-				return true;
+			if(p%2==1) {
+				return false;
 			}
 		}
-		return false;
+		return true;
 	}
 
 }
